c2d3ed9e2294da77ed28b59da1875df786e0de85
Adrian <adrianed19@gmail.com>
2025-01-26 11:56:06 +0100

    removed unnessisary code

--- sosialhub/src/components/Header.js
+++ sosialhub/src/components/Header.js
@@ -1,20 +1,3 @@
-/*import React from "react";
-
-function Header() {
-  return (
-    <header className="header">
-      <h1>Sosialhub</h1>
-      <ul>
-        <li>Home</li>
-        <li>Profile</li>
-        <li>Posts</li>
-      </ul>
-    </header>
-  );
-}
-
-export default Header;*/
-
 import React from "react";
 import { NavLink } from "react-router-dom";
 
